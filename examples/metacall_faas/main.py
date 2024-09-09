f0047e8585c5689dad9bad7167017243a9cbb64b
Jose Antonio Dominguez <chamot11@gmail.com>
2024-09-09 19:30:03 -0300

    Adds inspect to write on callee module.

--- examples/metacall_faas/main.py
+++ examples/metacall_faas/main.py
@@ -1,8 +1,4 @@
 from fasthtml_extn.deploy import deploy_metacall_faas
 
 # Initialize __all__ as an empty list
-__all__ = []
-
-deploy_metacall_faas(__all__)
-
-print(f"Exported functions: {__all__}")
+deploy_metacall_faas()
--- fasthtml_extn/deploy.py
+++ fasthtml_extn/deploy.py
@@ -1,7 +1,7 @@
+import inspect
 from fasthtml.core import to_xml
 from fasthtml_extn.libraries.tailwind import ExtnHtml
 from fasthtml_extn.utils import create_metacall_app, get_metacall_app_path
-
 
 pages = create_metacall_app()
 
@@ -17,19 +17,28 @@ def _with_route(path: str):
     return app
 
 
-def deploy_metacall_faas(_all_):
+def deploy_metacall_faas() -> list[str]:
     """
     Discover functions in the module and add them to _all_
     """
+    frame = inspect.currentframe()
+    if frame is None:
+        raise RuntimeError(
+            "Cannot deploy functions in the module. Please call this function from a module."
+        )
+    caller_frame = frame.f_back
+    caller_module = inspect.getmodule(caller_frame)
+
+    _all_ = []
     for page in pages:
         app = pages[page]
         route = app["route"]
         function_name = f"app{route}"
         print("Adding Function", function_name)
-        globals()[function_name] = _with_route(route)
+        setattr(caller_module, function_name, _with_route(route))
         if function_name not in _all_:
             _all_.append(function_name)
     if "app" not in _all_:
-        globals()["app"] = _with_route("")
+        setattr(caller_module, "app", _with_route(""))
         _all_.append("app")
     return _all_
